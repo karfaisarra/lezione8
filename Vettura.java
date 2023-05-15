5f5319d508c41bfcb70ebb1d352163e9f432ce29
karfaisarra <karfaisarra@gmail.com>
2023-05-15 16:14:04 +0200

    add exercice

---
+++ Vettura.java
@@ -0,0 +1,19 @@
+package lezione8;
+
+public class Vettura extends Veicolo{
+	private String tipologia;
+	
+	public Vettura(String targa, String marca, String modello, boolean guasto, String tipologia) {
+		super(targa, marca, modello, guasto);
+		this.tipologia=tipologia;
+	}
+
+	public String getTipologia() {
+		return tipologia;
+	}
+
+	public void setTipologia(String tipologia) {
+		this.tipologia = tipologia;
+	}
+
+}
